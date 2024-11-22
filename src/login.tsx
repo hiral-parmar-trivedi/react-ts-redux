db63f2f08b321519ccea85448d6cbd6b3a5c425a
Fs <fs@MacBook-Pro-5.local>
2024-11-22 15:16:19 +0530

    updated configuration

--- src/login.tsx
+++ src/login.tsx
@@ -8,11 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <p>Login file</p>
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
